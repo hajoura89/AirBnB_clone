e7d0e2f18391b1ec200b88f737f904e464bea28a
hajoura89 <hajarkammou@gmail.com>
2023-09-16 03:03:35 -0700

    Fixing Task 2 Errors

--- models/base_model.py
+++ models/base_model.py
@@ -8,6 +8,26 @@ from datetime import datetime
 class BaseModel:
     """Representation of BaseModel"""
 
+    def __init__(self, *args, **kwargs):
+        """Init a new BaseModel
+
+        Args:
+            *args (any): won’t be used
+            **kwargs (dict): Key/value of attributes
+        """
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        tformat = "%Y-%m-%dT%H:%M:%S.%f"
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tformat)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
+
     def __str__(self):
         """Print representation of the BaseModel"""
         clname = self.__class__.__name__
@@ -22,28 +42,8 @@ class BaseModel:
         """Returns a dictionary containing all keys/values
         of __dict__ of the BaseModel instance
         """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-
-    def __init__(self, *args, **kwargs):
-        """Init a new BaseModel
-
-        Args:
-            *args (any): won’t be used
-            **kwargs (dict): Key/value of attributes
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            models.storage.new(self)
+        obj_dict = self.__dict__.copy()
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        obj_dict["__class__"] = self.__class__.__name__
+        return obj_dict
